aa2cb295501ff600057915bc85d03285b222d9a1
RJ-08 <ranjitsamalarj@gmail.com>
2025-11-20 15:59:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-07860a2d7eb515d9a" 
+  ami           = "ami-0fa3fe0fa7920f68e" 
   instance_type = "t3.micro"
 
   tags = {
