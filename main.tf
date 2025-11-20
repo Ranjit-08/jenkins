d4c4adc4356d11eebe38267b0213dcfc202b392b
RJ-08 <ranjitsamalarj@gmail.com>
2025-11-20 15:34:01 +0530

    Add AWS provider and EC2 instance configuration

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
 
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-07860a2d7eb515d9a" 
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ranjit-ec2"
+  }
+}
