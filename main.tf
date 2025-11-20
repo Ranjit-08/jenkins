cdfa177b749f8b73dfcf0699a4bb8c9c7a267aef
RJ-08 <ranjitsamalarj@gmail.com>
2025-11-20 15:54:25 +0530

    Change EC2 instance type from t2.micro to t3.micro

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "my_ec2" {
   ami           = "ami-07860a2d7eb515d9a" 
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "ranjit-ec2"
